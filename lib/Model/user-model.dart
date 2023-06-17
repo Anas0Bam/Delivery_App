e17132cf74c0b86b2d0ffa8afe40359dae8c7087
azoooz-56 <azoooz-56@hotmail.com>
2023-06-17 22:51:32 +0300

    home screen

--- lib/Model/user-model.dart
+++ lib/Model/user-model.dart
@@ -1,3 +1,72 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class User{
+class UserAccount {
+  String _id;
+  String _name;
+  String _email;
+  String _phoneNumber;
+
+  UserAccount(
+      this._id,
+      this._name,
+      this._email,
+      this._phoneNumber,
+      );
+
+  String get id => _id;
+
+  set id(String value) {
+    _id = value;
+  }
+
+  String get name => _name;
+
+  set name(String value) {
+    _name = value;
+  }
+
+  String get phoneNumber => _phoneNumber;
+
+  set phoneNumber(String value) {
+    _phoneNumber = value;
+  }
+
+  String get email => _email;
+
+  set email(String value) {
+    _email = value;
+  }
+
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      "Email": _email.toString(),
+      "uid": _id.toString(),
+      "username": _name.toString(),
+      "phoneNumber": _phoneNumber.toString(),
+    };
+  }
+  //
+
+  factory UserAccount.fromFirestore(
+      DocumentSnapshot<Map<String, dynamic>> snapshot,
+      SnapshotOptions? options,
+      ) {
+    final data = snapshot.data();
+    return UserAccount(
+      data?["uid"],
+      data?["username"],
+      data?["Email"],
+      data?["phoneNumber"],
+    );
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      "id": id,
+      "name": name,
+      "email": email,
+      "phoneNumber": phoneNumber,
+    };
+  }
 }
--- lib/Screens/home-screen.dart
+++ lib/Screens/home-screen.dart
@@ -13,8 +13,9 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: colorBluishGray,
       floatingActionButton: FloatingActionButton(
+        backgroundColor: Color(0xFF32d951),
         onPressed: () {},
-        child: Container(),
+        child: Image.asset('assets/whats-logo.png',width: width*0.09,),
       ),
       body: SafeArea(
         child: Container(
@@ -130,6 +131,7 @@ class HomeScreen extends StatelessWidget {
               Container(
                 padding: EdgeInsets.only(left: width * 0.05, right: width * 0.05),
                 child: Row(
+
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Text(
@@ -147,95 +149,103 @@ class HomeScreen extends StatelessWidget {
                 height: height * 0.02,
               ),
               Container(
-                height: height * 0.30,
+                height: height * 0.28,
                 child: ListView.builder(
                   itemCount: placeList.length,
                   scrollDirection: Axis.horizontal,
                   itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      child: Column(
-                        children: [
-                          Container(
-                            alignment: Alignment.center,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.all(Radius.circular(15)),
-                              color: colorLightGray,
-                              border: Border.all(color: colorDarkGray),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.black.withOpacity(0.3), // Replace with your desired shadow color
-                                  blurRadius: 5, // Replace with your desired blur radius
-                                  spreadRadius: 0, // Set to 0 to restrict shadow to the bottom
-                                  offset: Offset(0, 8), // Adjust the offset for desired shadow position
-                                ),
-                              ],
-                            ),
-                            margin: EdgeInsets.only(left: width * 0.05, right: 2),
-                            padding: EdgeInsets.symmetric(vertical: height * 0.005, horizontal: width * 0.05),
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text("open", style: TextStyle(color: Color(0xFF49EE20), fontSize: 18)),
-                                Image.asset(
-                                  placeList[index].image,
-                                  height: height * 0.2,
-                                ),
-                                SizedBox(
-                                  width: width * 0.02,
-                                ),
-                                Container(
-                                  child: InkWell(
-                                    child: Container(
-                                      height: 50,
-                                      width: 45,
-                                      decoration: BoxDecoration(
-                                        color: colorVeryLightGray,
-                                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                                        boxShadow: [
-                                          BoxShadow(
-                                            color: Colors.grey.withOpacity(0.5), // Replace with your desired shadow color
-                                            spreadRadius: 1, // Replace with your desired spread radius
-                                            blurRadius: 5, // Replace with your desired blur radius
-                                            offset: Offset(0, 3), // Replace with your desired offset
-                                          ),
-                                        ],
-                                      ),
-                                      child: Icon(
-                                        Icons.favorite_border,
-                                        color: Color(0xFFFF9832),
-                                      ),
+                    return Column(
+                      children: [
+                        Container(
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(15)),
+                            color: colorLightGray,
+                            border: Border.all(color: colorDarkGray),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.3), // Replace with your desired shadow color
+                                blurRadius: 5, // Replace with your desired blur radius
+                                spreadRadius: 0, // Set to 0 to restrict shadow to the bottom
+                                offset: Offset(0, 8), // Adjust the offset for desired shadow position
+                              ),
+                            ],
+                          ),
+                          margin: EdgeInsets.only(left: width * 0.05, right: 2),
+                          padding: EdgeInsets.symmetric(vertical: height * 0.005, horizontal: width * 0.05),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("open", style: TextStyle(color: Color(0xFF49EE20), fontSize: 18)),
+                              Image.asset(
+                                placeList[index].image,
+                                height: height * 0.2,
+                              ),
+                              SizedBox(
+                                width: width * 0.02,
+                              ),
+                              Container(
+                                child: InkWell(
+                                  child: Container(
+                                    height: 50,
+                                    width: 45,
+                                    decoration: BoxDecoration(
+                                      color: colorVeryLightGray,
+                                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.grey.withOpacity(0.5), // Replace with your desired shadow color
+                                          spreadRadius: 1, // Replace with your desired spread radius
+                                          blurRadius: 5, // Replace with your desired blur radius
+                                          offset: Offset(0, 3), // Replace with your desired offset
+                                        ),
+                                      ],
+                                    ),
+                                    child: Icon(
+                                      Icons.favorite_border,
+                                      color: Color(0xFFFF9832),
                                     ),
                                   ),
-                                )
-                              ],
-                            ),
+                                ),
+                              )
+                            ],
                           ),
-                          Container(
-                            child: Column(
+                        ),
+                        SizedBox(height: 5,),
+                        Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
-                                Container(
-                                  child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Text(
-                                        placeList[index].name,
-                                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                                      ),
-                                      Row(
-                                        children: [Text(placeList[index].rate), Icon(Icons.star, color: Color(0xFFFF9832))],
-                                      )
-                                    ],
-                                  ),
+                                Text(
+                                  placeList[index].name,
+                                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                                ),
+                                SizedBox(width: width*0.25,),
+                                Row(
+                                  children: [Text(placeList[index].rate), Icon(Icons.star, color: Color(0xFFFF9832))],
                                 )
                               ],
-                            ),
-                          )
-                        ],
-                      ),
+                            )
+                          ],
+                        )
+                      ],
                     );
                   },
                 ),
               ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: width*0.05),
+                padding: EdgeInsets.all(15),
+                width: width,
+                height: height*0.15,
+                decoration: BoxDecoration(
+                  color: colorVeryLightGray,
+                  borderRadius: BorderRadius.all(Radius.circular(20))
+                ),
+                alignment: Alignment.center,
+                child: Text("اطلب فوق 200 ريال\n ولك توصيل مجاني",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25),),
+              )
             ],
           ),
         ),
